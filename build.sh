85316dea888ea722aea0818c9c1782bd1614e02a
Martin George <marty@dingopaws.net>
2024-10-25 12:32:33 +0100

    Added Native Mode

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 rm -rf build dist
-nicegui-pack --name "J2Live" --windowed src/j2live.py --osx-bundle-identifier 'dingo.j2live' --icon j2live.icns
+nicegui-pack --onefile --name "J2Live" --windowed src/j2live.py --osx-bundle-identifier 'dingo.j2live' --icon j2live.icns
 cp -r venv/lib/python3.12/site-packages/ansible dist/J2Live.app/Contents/Frameworks/ansible
+cp -r venv/lib/python3.12/site-packages/ansible dist/J2Live/_internal/ansible
